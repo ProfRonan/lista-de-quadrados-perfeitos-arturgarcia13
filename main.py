f6c3fae7cd16ccddfef00600504efb7a91948dda
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-14 10:25:03 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# recebe um número do usuário
+numTest = int(input("Digite um número: "))
+
+# imprima todos os quadrados perfeitos menores ou iguais a esse número
+i = 1
+while i <= numTest :
+    print(i**2)
+    i += 1
